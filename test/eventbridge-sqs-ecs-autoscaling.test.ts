81ef0de1e24866110d304033a7f0391cc329f6a0
VaughnGit <alvin@vaughns.net>
2023-07-25 13:11:09 -0500

    Initial commit

---
+++ test/eventbridge-sqs-ecs-autoscaling.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventbridgeSqsEcsAutoscaling from '../lib/eventbridge-sqs-ecs-autoscaling-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eventbridge-sqs-ecs-autoscaling-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventbridgeSqsEcsAutoscaling.EventbridgeSqsEcsAutoscalingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
